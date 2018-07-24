cf4e6b4304f70fb9d644eb7f340ce0100e2d3165
Anders.Eriksson <anders.eriksson@ef.com>
2018-07-25 00:10:35 +0800

    start work on menu

--- main.js
+++ main.js
@@ -1,11 +1,45 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 
-  function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({ width: 800, height: 600 })
 
-    // and load the index.html of the app.
-    win.loadFile('index.html')
-  }
+  // and load the index.html of the app.
+  win.loadFile('index.html')
 
-  app.on('ready', createWindow)
+  // open Developer Tools
+  win.webContents.openDevTools();
+
+  createMenu();
+}
+
+function createMenu() {
+  var menu = Menu.buildFromTemplate([
+    {
+      label: 'Menu',
+      submenu: [
+        {
+          label: 'About MurrMurr'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Preferences'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Quit MurrMurr',
+          accelerator: 'CmdOrCtrl+Q',
+          click() { app.quit() },
+        }
+      ]
+    }
+  ]);
+
+  Menu.setApplicationMenu(menu);
+}
+
+app.on('ready', createWindow)
